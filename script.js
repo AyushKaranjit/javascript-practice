4147c1b192d4ebb3d6a3f728dfde0abeacd7b694
Ayush Karanjit <aayushkaranjit2@gmail.com>
2024-10-02 18:24:53 +0545

    testets

--- script.js
+++ script.js
@@ -104,3 +104,11 @@
 //   .reduce((total, num) => total + num, 0);
 // console.log(sumOfTripledEvens); // Outputs 90
 // console.log(arr); // Outputs [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+let myIncomeInNpr = 15000;
+let myIncomeInDollar = myIncomeInNpr / 150;
+let myTaxRate = 0.1;
+let myTaxes = myIncomeInDollar * myTaxRate;
+console.log(myTaxes);
+console.log(myIncomeInDollar);
+console.log(myIncomeInNpr);
+console.log(myTaxRate);
